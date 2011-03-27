5c65be24298dd40c7ce956124bd9f0a4ba746911
Jacob Evans <jacob@dekz.net>
2011-03-27 21:03:49 +1000

    not getting message from args..

--- src/drsa.cc
+++ src/drsa.cc
@@ -38,28 +38,45 @@ Handle<Value> DRSA::RSAEncrypt(const Arguments &args) {
   // ssize_t len = DecodeBytes(args[0], BINARY);
   // char *pem_pub = new char[len];
   Local<Object> pub_obj = args[0]->ToObject();
-  unsigned char *pem_pub = (unsigned char*) Buffer::Data(pub_obj);
-  ssize_t pub_len = Buffer::Length(pub_obj);
+  char *pem_pub = Buffer::Data(pub_obj);
+  size_t pub_len = Buffer::Length(pub_obj);
 
   Local<Object> msg_obj = args[1]->ToObject();
-  unsigned char *msg = (unsigned char*)Buffer::Data(msg_obj);
-  ssize_t msg_len = Buffer::Length(msg_obj);
+  char *msg = Buffer::Data(msg_obj);
+  size_t msg_len = Buffer::Length(msg_obj);
   
 
-  if (pub_len < 0) return ThrowException(Exception::TypeError(String::New("Bad length of key")));
+  if (pub_len < 0) {
+    return ThrowException(Exception::TypeError(String::New("Bad length of key"))); 
+  }
+
+  if (msg_len < 0) {
+    return ThrowException(Exception::TypeError(String::New("Bad length of msg")));
+  }
 
   RSA *rsa_pub = RSA_new();
 
-  if(!BIO_write(rsa_bio, pem_pub, pub_len)) ThrowException(Exception::TypeError(String::New("Bad write of key")));
+  if(!BIO_write(rsa_bio, pem_pub, pub_len)) return ThrowException(Exception::TypeError(String::New("Bad write of key")));
+  fprintf(stderr, "%s\n", pem_pub);
+  fprintf(stderr, "%s\n", msg);
 
    //if this doesn't work use pkey
-  int ok = PEM_write_bio_RSAPublicKey(rsa_bio, rsa_pub);
+  rsa_pub = PEM_read_bio_RSAPublicKey(rsa_bio, NULL, NULL, 0);
+  if (!rsa_pub) {
+    return ThrowException(Exception::TypeError(String::New("Error getting PEM encoded key")));
+  }
+
+  // rsa_pub = EVP_PKEY_get1_RSA(pkey);
   //should work out the block size to properly allocate
   unsigned char enc[2560] = { 0 };
-  int written = RSA_public_encrypt(msg_len, msg, enc, rsa_pub, RSA_PKCS1_OAEP_PADDING);
+  int written = RSA_public_encrypt(msg_len, (unsigned char*) msg, enc, rsa_pub, RSA_PKCS1_OAEP_PADDING);
+  fprintf(stderr, "%s\n", enc);
+
+  Local<Value> outString = Encode(enc, written, BINARY);
   EVP_PKEY_free(pkey);
   RSA_free(rsa_pub);
   BIO_free(rsa_bio);
+  return scope.Close(outString);
 }
 
 DRSA::DRSA() : ObjectWrap() {
